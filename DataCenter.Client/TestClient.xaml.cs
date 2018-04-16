fb7d9189ca42ee902be1b33818177b52770708b3
bob wang <bob@hotmail.com>
2018-04-16 21:58:26 +0100

    adding client

---
+++ DataCenter.Client/TestClient.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DataCenter.Proxy;
+using DataCenter.Access;
+
+namespace DataCenter.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class TestClient : Window
+    {
+        public TestClient()
+        {
+            InitializeComponent();
+        }
+
+        private void GetClicked(object sender, RoutedEventArgs e)
+        {
+            var client = new DataClient();
+            int i = int.Parse(param.Text);
+            var trades = client.GetData(i);
+            result.Content = string.Empty;
+            foreach (var t in trades)
+            {
+                result.Content += string.Format("{0}: {1} £{2} of {3}\n", t.ID, t.IsBuy ? "Buy" : "Sell", t.Amount, t.MarketName);
+            }
+            client.Close();
+        }
+    }
+}
